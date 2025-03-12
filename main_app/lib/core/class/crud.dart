a1094f916bd50b28f8be08040ff9251a739accb3
Ayman <blidiayman47@gmail.com>
2025-03-13 00:51:59 +0100

    edit crud to handle the list response from the GET request

--- main_app/lib/core/class/crud.dart
+++ main_app/lib/core/class/crud.dart
@@ -4,48 +4,33 @@ import 'package:http/http.dart' as http;
 
 class Crud {
   Future<Map> get({required String url, String queryPar = ''}) async {
-    return await sendRequest(
-      () => http.get(Uri.parse(url + queryPar)),
-    );
+    return await sendRequest(() => http.get(Uri.parse(url + queryPar)), isGet: true);
   }
 
-  Future<Map> post({
-    required String url,
-    Object? body,
-    String queryPar = '',
-  }) async {
-    return await sendRequest(
-      () => http.post(Uri.parse(url + queryPar), body: body),
-    );
+  Future<Map> post({required String url, Object? body, String queryPar = ''}) async {
+    return await sendRequest(() => http.post(Uri.parse(url + queryPar), body: body));
   }
 
-  Future<Map> patch({
-    required String url,
-    Object? body,
-    String queryPar = '',
-  }) async {
-    return await sendRequest(
-      () => http.patch(Uri.parse(url + queryPar), body: body),
-    );
+  Future<Map> patch({required String url, Object? body, String queryPar = ''}) async {
+    return await sendRequest(() => http.patch(Uri.parse(url + queryPar), body: body));
   }
 
-  Future<Map> delete({
-    required String url,
-    Object? body,
-    String queryPar = '',
-  }) async {
-    return await sendRequest(
-      () => http.delete(Uri.parse(url + queryPar), body: body),
-    );
+  Future<Map> delete({required String url, Object? body, String queryPar = ''}) async {
+    return await sendRequest(() => http.delete(Uri.parse(url + queryPar), body: body));
   }
 
-  Future<Map> sendRequest(Future<http.Response> Function() request) async {
+  Future<Map> sendRequest(Future<http.Response> Function() request, {bool isGet = false}) async {
     try {
       if (await checkInternet()) {
         http.Response response = await request();
         print('=======respons============${response.body}======');
 
         if (response.statusCode < 300) {
+          // if (isGet) {
+          if (response.request!.method == 'GET') {
+            List responsebody = jsonDecode(response.body);
+            return {'data': responsebody};
+          }
           Map responsebody = jsonDecode(response.body);
           return responsebody;
         } else {
@@ -61,11 +46,4 @@ class Crud {
   }
 }
 
-enum StatusRequest {
-  loading,
-  success,
-  failed,
-  internetFailed,
-  serverFailed,
-  exceptionFailed,
-}
+enum StatusRequest { loading, success, failed, internetFailed, serverFailed, exceptionFailed }
