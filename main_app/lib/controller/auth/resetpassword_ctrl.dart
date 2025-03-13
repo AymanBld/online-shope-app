9178cd0155a567d993c26f05d8c8974605e4fa0f
Ayman <blidiayman47@gmail.com>
2025-03-13 03:22:09 +0100

    relink password reset

--- main_app/lib/controller/auth/resetpassword_ctrl.dart
+++ main_app/lib/controller/auth/resetpassword_ctrl.dart
@@ -1,12 +1,13 @@
 import 'package:online_shope_app/core/class/crud.dart';
+import 'package:online_shope_app/core/constant/links.dart';
 import 'package:online_shope_app/core/constant/routes.dart';
 import 'package:online_shope_app/core/functions/handle_statuss.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ResetPasswordCtrl extends GetxController {
-  late TextEditingController password;
-  late TextEditingController resetpassword;
+  late TextEditingController password1;
+  late TextEditingController password2;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   StatusRequest? statusrequest;
@@ -16,43 +17,44 @@ class ResetPasswordCtrl extends GetxController {
 
   resetPass() async {
     if (formKey.currentState!.validate()) {
-      if (password.text == resetpassword.text) {
+      if (password1.text == password2.text) {
         statusrequest = StatusRequest.loading;
         update();
 
-        Map response = await crud.post(url: '');
+        Map response = await crud.post(
+          url: AppLinks.resetPassword,
+          body: {
+            if (username.isEmail) 'email': username,
+            if (!username.isEmail) 'username': username,
+            'password': password2.text,
+          },
+        );
         statusrequest = handlingStatus(response);
         update();
 
         if (statusrequest == StatusRequest.success) {
           Get.offAllNamed(AppRoutes.success);
         } else if (statusrequest == StatusRequest.failed) {
-          Get.defaultDialog(
-            title: 'warning',
-            content: const Text('Email or Phone allredy existe!'),
-          );
+          Get.defaultDialog(title: 'error', content: Text(response['error']));
         }
       } else {
-        Get.defaultDialog(
-          title: 'warning',
-          content: const Text('Verify your new password'),
-        );
+        Get.defaultDialog(title: 'warning', content: const Text('Verify your new password'));
       }
     }
   }
 
   @override
   void onInit() {
-    password = TextEditingController();
-    resetpassword = TextEditingController();
+    password1 = TextEditingController();
+    password2 = TextEditingController();
     username = Get.arguments['username'];
     super.onInit();
   }
 
   @override
   void onClose() {
-    resetpassword.dispose();
-    password.dispose();
+    password1.dispose();
+    password2.dispose();
     super.onClose();
   }
 }
