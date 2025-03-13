aaecb8af916410f1dcca972fbf26fb625265d004
Ayman <blidiayman47@gmail.com>
2025-03-13 03:13:45 +0100

    relink check email

--- main_app/lib/controller/auth/checkemail_ctrl.dart
+++ main_app/lib/controller/auth/checkemail_ctrl.dart
@@ -1,11 +1,12 @@
 import 'package:online_shope_app/core/class/crud.dart';
+import 'package:online_shope_app/core/constant/links.dart';
 import 'package:online_shope_app/core/constant/routes.dart';
 import 'package:online_shope_app/core/functions/handle_statuss.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CheckEmailCtrl extends GetxController {
-  late TextEditingController email;
+  late TextEditingController username;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   Crud crud = Get.find<Crud>();
 
@@ -16,33 +17,33 @@ class CheckEmailCtrl extends GetxController {
       statusrequest = StatusRequest.loading;
       update();
 
-      Map response = await crud.post(url: '');
+      Map response = await crud.post(
+        url: AppLinks.checkEmail,
+        body: {
+          if (username.text.isEmail) 'email': username.text,
+          if (!username.text.isEmail) 'username': username.text,
+        },
+      );
       statusrequest = handlingStatus(response);
       update();
 
       if (statusrequest == StatusRequest.success) {
-        Get.offAllNamed(
-          AppRoutes.checkCode,
-          arguments: {'email': email.text, 'where': 'reset'},
-        );
+        Get.offAllNamed(AppRoutes.checkCode, arguments: {'username': username.text, 'where': 'reset'});
       } else if (statusrequest == StatusRequest.failed) {
-        Get.defaultDialog(
-          title: 'warning',
-          content: const Text('Email not existe!'),
-        );
+        Get.defaultDialog(title: 'Error', content: Text(response['error']));
       }
     }
   }
 
   @override
   void onInit() {
-    email = TextEditingController();
+    username = TextEditingController();
     super.onInit();
   }
 
   @override
   void onClose() {
-    email.dispose();
+    username.dispose();
     super.onClose();
   }
 }
---
+++ main_app/lib/view/screens/auth/check_email.dart
@@ -0,0 +1,59 @@
+import 'package:online_shope_app/controller/auth/checkemail_ctrl.dart';
+import 'package:online_shope_app/core/class/handling_states.dart';
+import 'package:online_shope_app/core/constant/colors.dart';
+import 'package:online_shope_app/core/functions/validatore.dart';
+import 'package:online_shope_app/view/widgets/onboarding/app_bar.dart';
+import 'package:online_shope_app/view/widgets/auth/custum_field.dart';
+import 'package:online_shope_app/view/widgets/auth/header.dart';
+import 'package:online_shope_app/view/widgets/auth/button.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CheckEmail extends StatelessWidget {
+  const CheckEmail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Get.put(CheckEmailCtrl());
+
+    return Scaffold(
+      backgroundColor: AppColor.backgroundColor,
+      appBar: CustumAppBar(title: 'check t'.tr),
+      body: GetBuilder<CheckEmailCtrl>(
+        builder:
+            (controller) => HandlingStates(
+              status: controller.statusrequest,
+              widget: Padding(
+                padding: const EdgeInsets.all(30),
+                child: Form(
+                  key: controller.formKey,
+                  child: ListView(
+                    children: [
+                      Header(title: 'check t'.tr, subtitle: 'check s'.tr),
+                      const SizedBox(height: 60),
+                      CustumTextField(
+                        control: controller.username,
+                        type: TextInputType.name,
+                        valid: (val) {
+                          return validate(val!, 'username');
+                        },
+                        hint: 'Enter Your Email or Username',
+                        label: 'Username',
+                        icon: Icons.email_outlined,
+                      ),
+                      const SizedBox(height: 15),
+                      CustumButton(
+                        text: 'check'.tr,
+                        ontap: () {
+                          controller.checkEmail();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+      ),
+    );
+  }
+}
