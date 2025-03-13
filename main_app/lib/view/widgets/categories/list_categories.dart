628708e6f904822d454a03dbf349f8766810ccb5
Ayman <blidiayman47@gmail.com>
2025-03-13 01:00:22 +0100

    edit the CategoryModel for the new responses

--- main_app/lib/view/widgets/categories/list_categories.dart
+++ main_app/lib/view/widgets/categories/list_categories.dart
@@ -5,40 +5,37 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ListCats extends StatelessWidget {
-  const ListCats({
-    super.key,
-  });
+  const ListCats({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetBuilder<CategoriesCtrl>(
-      builder: (controller) => SizedBox(
-        height: 50,
-        child: ListView.separated(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          scrollDirection: Axis.horizontal,
-          itemCount: controller.categories.length,
-          separatorBuilder: (context, index) => const SizedBox(width: 20),
-          itemBuilder: (context, index) {
-            CategoriesModel cat = CategoriesModel.fromJson(controller.categories[index]);
-            return InkWell(
-              onTap: () {
-                controller.changeCat(cat.categorieId!);
+      builder:
+          (controller) => SizedBox(
+            height: 50,
+            child: ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              scrollDirection: Axis.horizontal,
+              itemCount: controller.categories.length,
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
+              itemBuilder: (context, index) {
+                CategoryModel cat = CategoryModel.fromJson(controller.categories[index]);
+                return InkWell(
+                  onTap: () {
+                    controller.changeCat(cat.id!);
+                  },
+                  child: Text(
+                    cat.name!,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: cat.id == controller.selectedcat ? AppColor.primryColor : Colors.black,
+                    ),
+                  ),
+                );
               },
-              child: Text(
-                cat.categorieName!,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: cat.categorieId == controller.selectedcat
-                      ? AppColor.primryColor
-                      : Colors.black,
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+            ),
+          ),
     );
   }
 }
--- main_app/lib/view/widgets/home/list_categories.dart
+++ main_app/lib/view/widgets/home/list_categories.dart
@@ -5,41 +5,37 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ListCategories extends GetView<HomeCtrl> {
-  const ListCategories({
-    super.key,
-  });
+  const ListCategories({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 120,
       child: ListView.separated(
-          scrollDirection: Axis.horizontal,
-          itemCount: controller.categories.length,
-          separatorBuilder: (context, index) => const SizedBox(width: 10),
-          itemBuilder: (context, index) {
-            CategoriesModel cat = CategoriesModel.fromJson(controller.categories[index]);
-            return InkWell(
-              onTap: () {
-                controller.onTapCat(cat.categorieId!);
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: AppColor.primryColorDark,
-                    ),
-                    height: 70,
-                    width: 70,
-                    child: const Icon(Icons.laptop, color: Colors.white, size: 40),
-                  ),
-                  Text(cat.categorieName!),
-                ],
-              ),
-            );
-          }),
+        scrollDirection: Axis.horizontal,
+        itemCount: controller.categories.length,
+        separatorBuilder: (context, index) => const SizedBox(width: 10),
+        itemBuilder: (context, index) {
+          CategoryModel cat = CategoryModel.fromJson(controller.categories[index]);
+          return InkWell(
+            onTap: () {
+              controller.onTapCat(cat.id!);
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: AppColor.primryColorDark),
+                  height: 70,
+                  width: 70,
+                  child: const Icon(Icons.laptop, color: Colors.white, size: 40),
+                ),
+                Text(cat.name!),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
